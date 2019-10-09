70fbf25458a2b8fadc64106c63c4e514920f68be
Unknown <benjamin.strabach@live.fr>
2019-10-09 15:20:11 +0200

    Bouclage du menu

--- src/bo/SegaBank.java
+++ src/bo/SegaBank.java
@@ -35,62 +35,85 @@ public class SegaBank {
 	
 	public static void main(String[] args) {
 		int response;
-		boolean first = true;
+		boolean first;
+		boolean run = true;
 		
 		System.out.println("Bienvenue dans l'application de gestion SegaBank\n");
-		
-		do {
-			if ( !first )
-				System.out.println( "Mauvais choix... merci de recommencer !" );
-			new ConsoleMessageCRUD();
-			try {
-				response = sc.nextInt();
-			} catch ( InputMismatchException e ) {
-				response = -1;
-			} finally {
-				sc.nextLine();
-			}
-			first = false;
-		}while(response < 1 || response > 11);
-		
-		switch (response) {
-		case 1:
-			// TO DO ajouter une nouvelle agence
-			Agence agence = ajoutAgence();
-			break;
-		case 2:
-			// TO DO modifier une agence existante
-			break;
-		case 3:
-			// TO DO supprimer une agence existante
-			break;
-		case 4:
-			// TO DO lister les agences
-			break;
-		case 5:
-			// TO DO sauvegarder votre liste d'agence
-			break;
-		case 6:
-			// TO DO ajouter un nouveau compte
-			System.out.println( " - AJOUT D'UN NOUVEAU COMPTE - " );
+		do{
+			first = true;
+			do {
+				if ( !first )
+					System.out.println( "Entrée invalide... merci de recommencer !" );
+				new ConsoleMessageCRUD();
+				try {
+					response = sc.nextInt();
+				} catch ( InputMismatchException e ) {
+					response = -1;
+				} finally {
+					sc.nextLine();
+				}
+				first = false;
+			}while(response < 1 || response > 11);
 
-			break;
-		case 7:
-			// TO DO modifier un compte existant
-			break;
-		case 8:
-			// TO DO supprimer un compte existant
-			break;
-		case 9:
-			// TO DO lister les comptes
-			break;
-		case 10:
-			// TO DO sauvegarder l'etat des comptes
-			break;
-		case 11:
-			// TO DO quitter
-			break;
-		}
+			switch (response) {
+				case 1:
+					// TO DO ajouter une nouvelle agence
+					Agence agence = ajoutAgence();
+					break;
+				case 2:
+					// TO DO modifier une agence existante
+					break;
+				case 3:
+					// TO DO supprimer une agence existante
+					break;
+				case 4:
+					// TO DO lister les agences
+					break;
+				case 5:
+					// TO DO sauvegarder votre liste d'agence
+					break;
+				case 6:
+					// TO DO ajouter un nouveau compte
+					System.out.println( " - AJOUT D'UN NOUVEAU COMPTE - " );
+
+					break;
+				case 7:
+					// TO DO modifier un compte existant
+					break;
+				case 8:
+					// TO DO supprimer un compte existant
+					break;
+				case 9:
+					// TO DO lister les comptes
+					break;
+				case 10:
+					// TO DO sauvegarder l'etat des comptes
+					break;
+				case 11:
+					// TO DO quitter
+					System.out.println( " - ARRET DU PROGRAMME - " );
+					run = false;
+			}
+		}while(run == true);
 	}
 
+	public static Agence ajoutAgence() {
+		System.out.println( " - AJOUT D'UNE NOUVELLE AGENCE - " );
+		System.out.println( "Entrer un ID : " );
+		int id = sc.nextInt();
+		System.out.println( "Entrer le code de l'agence : " );
+		String code = sc.next();
+		System.out.println( "Entrer l'adresse de l'agence : " );
+		System.out.println( "	Nom de la rue : " );
+		String addrRue = sc.next();
+		System.out.println( "	Numéro dans la rue : " );
+		String addrNum = sc.next();
+		System.out.println( "	Code postal : " );
+		String addrCPostal = sc.next();
+		System.out.println( "	Ville : " );
+		String addrVille = sc.next();
+		Adresse adresse = new Adresse(addrNum, addrRue, addrVille, addrCPostal);
+		Agence agence = new Agence(code, id, adresse);
+		return agence;
+	}
 }
